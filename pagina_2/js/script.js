3db308bd33553907a0c703c9041a4b01812e7899
DevNeres <149329464+adsondev06@users.noreply.github.com>
2024-04-15 20:37:00 -0300

    teste

--- pagina_2/js/script.js
+++ pagina_2/js/script.js
@@ -1,39 +1,26 @@
-// const summaryDiv = document.getElementById("summary");
-// const sendToWhatsAppButton = document.getElementById("sendToWhatsAppButton");
+const summaryDiv = document.getElementById("summary");
+const sendToWhatsAppButton = document.getElementById("sendToWhatsAppButton");
 
-// // Carregar os códigos lidos armazenados em localStorage
-// const detectedBarcodes = JSON.parse(localStorage.getItem("detectedBarcodes")) || [];
+// Carregar os códigos lidos armazenados em localStorage
+const detectedBarcodes = JSON.parse(localStorage.getItem("detectedBarcodes")) || [];
 
-// // Exibir os códigos lidos numerados
-// detectedBarcodes.forEach((code, index) => {
-//   const codeDiv = document.createElement("div");
-//   codeDiv.textContent = (index + 1) + ":  " + code;
-//   summaryDiv.appendChild(codeDiv);
-// });
+// Exibir os códigos lidos numerados
+detectedBarcodes.forEach((code, index) => {
+  const codeDiv = document.createElement("div");
+  codeDiv.textContent = (index + 1) + ":  " + code;
+  summaryDiv.appendChild(codeDiv);
+});
 
-// function sendToWhatsApp() {
-//   // Construir a mensagem com os códigos numerados
-//   let message = "";
-//   detectedBarcodes.forEach((code, index) => {
-//     message += (index + 1) + ": Código: " + code + "\n";
-//   });
+function sendToWhatsApp() {
+  // Construir a mensagem com os códigos numerados
+  let message = "";
+  detectedBarcodes.forEach((code, index) => {
+    message += (index + 1) + ": Código: " + code + "\n";
+  });
 
-//   // URL do WhatsApp com a mensagem
-//   const whatsappURL = "https://api.whatsapp.com/send?text=" + encodeURIComponent(message);
-//   // Abrir a conversa no WhatsApp
-//   window.open(whatsappURL, "_blank");
-// }
+  // URL do WhatsApp com a mensagem
+  const whatsappURL = "https://api.whatsapp.com/send?text=" + encodeURIComponent(message);
+  // Abrir a conversa no WhatsApp
+  window.open(whatsappURL, "_blank");
+}
 
-// Fazer uma solicitação GET para obter os códigos do servidor
-fetch('http://localhost:3000/api/codes')
-  .then(response => response.json())
-  .then(data => {
-    const detectedBarcodes = data.codes || [];
-    // Atualizar a exibição com os códigos obtidos do servidor
-    detectedBarcodes.forEach((code, index) => {
-      const codeDiv = document.createElement("div");
-      codeDiv.textContent = (index + 1) + ": " + code;
-      summaryDiv.appendChild(codeDiv);
-    });
-  })
-  .catch(error => console.error('Erro ao obter os códigos:', error));
