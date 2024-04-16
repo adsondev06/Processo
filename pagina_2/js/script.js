136c30b6118fc40d7a72c8ce6c0804b140640cd5
DevNeres <149329464+adsondev06@users.noreply.github.com>
2024-04-16 01:00:07 -0300

    final

--- pagina_2/js/script.js
+++ pagina_2/js/script.js
@@ -1,45 +1,8 @@
-// const summaryDiv = document.getElementById("summary");
-// const sendToWhatsAppButton = document.getElementById("sendToWhatsAppButton");
-
-// // Carregar os códigos lidos armazenados em localStorage
-// const detectedBarcodes = JSON.parse(localStorage.getItem("detectedBarcodes")) || [];
-
-// // Exibir os códigos lidos numerados
-// detectedBarcodes.forEach((code, index) => {
-//   const codeDiv = document.createElement("div");
-//   codeDiv.textContent = (index + 1) + ":  " + code;
-//   summaryDiv.appendChild(codeDiv);
-// });
-
-// function sendToWhatsApp() {
-//   // Construir a mensagem com os códigos numerados
-//   let message = "";
-//   detectedBarcodes.forEach((code, index) => {
-//     message += (index + 1) + ": Código: " + code + "\n";
-//   });
-
-//   // URL do WhatsApp com a mensagem
-//   const whatsappURL = "https://api.whatsapp.com/send?text=" + encodeURIComponent(message);
-//   // Abrir a conversa no WhatsApp
-//   window.open(whatsappURL, "_blank");
-// }
-
 const summaryDiv = document.getElementById("summary");
 const sendToWhatsAppButton = document.getElementById("sendToWhatsAppButton");
-const messageInput = document.getElementById("messageInput");
 
 // Carregar os códigos lidos armazenados em localStorage
 const detectedBarcodes = JSON.parse(localStorage.getItem("detectedBarcodes")) || [];
-
-// Exibir a mensagem do input
-const message = messageInput.value;
-const messageDiv = document.createElement("div");
-messageDiv.textContent = message;
-summaryDiv.appendChild(messageDiv);
-
-// Adicionar duas quebras de linha entre a mensagem e os códigos
-summaryDiv.appendChild(document.createElement("br"));
-summaryDiv.appendChild(document.createElement("br"));
 
 // Exibir os códigos lidos numerados
 detectedBarcodes.forEach((code, index) => {
@@ -50,13 +13,14 @@ detectedBarcodes.forEach((code, index) => {
 
 function sendToWhatsApp() {
   // Construir a mensagem com os códigos numerados
-  let finalMessage = message + "\n\n";
+  let message = "";
   detectedBarcodes.forEach((code, index) => {
-    finalMessage += (index + 1) + ": Código: " + code + "\n";
+    message += (index + 1) + ": Código: " + code + "\n";
   });
 
   // URL do WhatsApp com a mensagem
-  const whatsappURL = "https://api.whatsapp.com/send?text=" + encodeURIComponent(finalMessage);
+  const whatsappURL = "https://api.whatsapp.com/send?text=" + encodeURIComponent(message);
   // Abrir a conversa no WhatsApp
   window.open(whatsappURL, "_blank");
 }
+
