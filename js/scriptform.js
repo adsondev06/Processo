a0dff4610f9417825269f57da3252e0f9ebdbf40
DevNeres <149329464+adsondev06@users.noreply.github.com>
2024-04-27 23:36:00 -0300

    teste

--- js/scriptform.js
+++ js/scriptform.js
@@ -1,10 +1,5 @@
-// Pegar os parâmetros enviados via POST
 const urlParams = new URLSearchParams(window.location.search);
-const nome = urlParams.get('nome');
-const codigos = JSON.parse(urlParams.get('codigos'));
+        const codigos = urlParams.get('codigos');
 
-// Preencher o campo de nome
-document.getElementById('nome').value = nome;
-
-// Preencher o textarea com os códigos
-document.getElementById('codigos').value = codigos.join('\n');
+        // Preencher o textarea com os códigos lidos
+        document.getElementById('codigos').value = codigos;
