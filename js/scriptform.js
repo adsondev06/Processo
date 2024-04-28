0d2d09ced3cb60e7fcb70a3b95917478510e6eb7
DevNeres <149329464+adsondev06@users.noreply.github.com>
2024-04-28 01:29:26 -0300

    teste

--- js/scriptform.js
+++ js/scriptform.js
@@ -8,36 +8,4 @@ const urlParams = new URLSearchParams(window.location.search);
         }
         //Meu código funciona certo acima  
 
-        document.getElementById('form').addEventListener('submit', function(event) {
-            event.preventDefault(); // Impede o envio padrão do formulário
-        
-            // Obtém os valores dos campos nome e codigos
-            const nome = document.getElementById('nome').value;
-            const codigos = document.getElementById('codigos').value;
-        
-            // Verifica se ambos os campos estão preenchidos
-            if (nome.trim() === '' || codigos.trim() === '') {
-                // Se algum dos campos não estiver preenchido, exibe uma mensagem de alerta
-                alert('Por favor, preencha todos os campos.');
-            } else {
-                // Se os campos estiverem preenchidos, envia os dados para o servidor
-                fetch(this.action, {
-                    method: this.method,
-                    body: new FormData(this)
-                })
-                .then(response => {
-                    if (response.ok) {
-                        // Se a resposta estiver OK, redireciona para a página de sucesso
-                        window.location.href = "index_sucesso.html";
-                    } else {
-                        // Se houver um erro, exibe uma mensagem de erro
-                        alert('Erro ao enviar os dados.');
-                    }
-                })
-                .catch(error => {
-                    console.error('Erro ao enviar os dados:', error);
-                    alert('Erro ao enviar os dados.');
-                });
-            }
-        });
         
